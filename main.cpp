0ea0ca87df3fa530d37e792e7a052263dbf69272
NikitaNet <102032304+NekichNet@users.noreply.github.com>
2024-09-18 13:47:59 +0500

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int main() {
+	SetConsoleOutputCP(1251);
+
+	double operand1;
+	double operand2;
+	char action;
+
+	std::cout << "Введите первый операнд: ";
+	std::cin >> operand1;
+	std::cout << "Введите оператор: ";
+	std::cin >> action;
+	std::cout << "Введите второй операнд: ";
+	std::cin >> operand2;
+
+	if (action == '+') {
+		std::cout << operand1 + operand2;
+	}
+	else if (action == '-') {
+		std::cout << operand1 - operand2;
+	}
+	else if (action == '*') {
+		std::cout << operand1 * operand2;
+	}
+	else if ((action == '/' || action == '%') && operand2 == 0.0) {
+		std::cout << "Ошибка!";
+	}
+	else if (action == '/') {
+		std::cout << operand1 / operand2;
+	}
+	else if (action == '^') {
+		std::cout << pow(operand1, operand2);
+	}
+	else if (action == '%') {
+		std::cout << operand1 / operand2 * 100 << "%";
+	}
+	else if (action == '|') {
+		std::cout << pow(operand1, 1 / operand2);
+	}
+	else {
+		std::cout << "Неизвестный оператор!";
+	}
+
+	return 0;
+}
